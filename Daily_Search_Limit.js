c5517d93bb61f173ccf7a895e960b68b8f2591a9
Gayan Karunathilaka <121455967+mprgc@users.noreply.github.com>
2025-11-10 13:07:06 +0530

    Update Daily_Search_Limit.js

--- Daily_Search_Limit.js
+++ Daily_Search_Limit.js
@@ -4,7 +4,7 @@
 // ======================================
 
 // 🧩 Settings
-const DAILY_LIMIT = 10;
+const DAILY_LIMIT = 4;
 const STORAGE_KEY = "gkict_search_count";
 const DATE_KEY = "gkict_search_date";
 
@@ -124,4 +124,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
