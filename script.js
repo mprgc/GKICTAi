37d985f8f2ff21b3de6fd6dca988adbdc40b8f09
Gayan Karunathilaka <121455967+mprgc@users.noreply.github.com>
2025-11-21 11:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 
 // API Setup
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxCmzE0piIj8ttK3gGuNNu2y6JOA091zmE7D4_PNrBUpc2xl87Mo2-pUZaWKvGo-7lN/exec";
+
+console.log(getValue());
+const API_URL = getValue();
+
 
 let controller, typingInterval;
 const chatHistory = [];
@@ -183,8 +186,3 @@ document.addEventListener("click", ({ target }) => {
 // Add event listeners for form submission and file input click
 promptForm.addEventListener("submit", handleFormSubmit);
 promptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click());
-
-
-
-
-
