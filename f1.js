570b3896dd2f33032461daf2977e77cc0d6ea38b
Gayan Karunathilaka <121455967+mprgc@users.noreply.github.com>
2025-11-21 11:36:49 +0530

    Add files via upload

---
+++ f1.js
@@ -0,0 +1,21 @@
+/*function doGet(e) {
+  // ඔබට මෙතනින් API URL එක set කරන්න
+  const apiKey = "AIzaSyDbqITHLeMX7XCenp5RGRV0CAlL_9_IfSk";
+  var apiUrl = "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=" + apiKey; // ← මෙය ඔබට වෙනස් කරන්න
+
+  var output = { apiUrl: apiUrl };
+
+  return ContentService
+          .createTextOutput(JSON.stringify(output))
+          .setMimeType(ContentService.MimeType.JSON);
+}*/
+
+  function getValue(e) {
+
+
+    const apiKey = "AIzaSyDbqITHLeMX7XCenp5RGRV0CAlL_9_IfSk";
+    var apiUrl = "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=" + apiKey; // ← මෙය ඔබට වෙනස් කරන්න
+
+  
+  return apiUrl;
+}
